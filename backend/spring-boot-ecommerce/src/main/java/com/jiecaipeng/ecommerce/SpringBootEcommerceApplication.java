ebfc07c34a15a7a349b7c4cea5d97d6d42ad36cf
Jiecai Peng <mci@fastmail.com>
2022-06-15 12:28:40 -0400

    Generated spring boot project

---
+++ backend/spring-boot-ecommerce/src/main/java/com/jiecaipeng/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jiecaipeng.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
