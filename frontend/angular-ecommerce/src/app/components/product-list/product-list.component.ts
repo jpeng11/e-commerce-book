aa6fc0b92b641b62636831225a0bb7525c468ed1
Jiecai Peng <mci@fastmail.com>
2022-07-07 17:52:18 -0400

    Call addtoCart from product-list component

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -16,7 +18,8 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
 
   ngOnInit() {
@@ -64,5 +67,9 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  addToCart(theProduct: Product) {}
+  addToCart(theProduct: Product) {
+    const cartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(cartItem);
+  }
 }
