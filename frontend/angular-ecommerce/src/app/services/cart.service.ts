09add7bea0cc7d7e3195056d19d9cddd31e75e5c
Jiecai Peng <mci@fastmail.com>
2022-07-07 17:49:31 -0400

    Generated cart item class and cart services

---
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() {}
+
+  addToCart(cartItem: CartItem) {
+    // check if we already have the item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0) {
+      // find the item in the cart based on item id
+      for (const tempCartItem of this.cartItems) {
+        if (tempCartItem.id === cartItem.id) {
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+
+      // check if we found it
+      alreadyExistsInCart = existingCartItem != undefined;
+    }
+
+    if (alreadyExistsInCart) {
+      existingCartItem.quantity++;
+    } else {
+      this.cartItems.push(cartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+    let totalPriceInCart: number = 0;
+    let totalQuantityInCart: number = 0;
+
+    for (const currentCartItem of this.cartItems) {
+      totalPriceInCart += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityInCart += currentCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceInCart);
+    this.totalQuantity.next(totalQuantityInCart);
+  }
+}
